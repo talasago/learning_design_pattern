e26144f3dd81079cd10fd76771f4bb960b2b95e7
talasago <lyricaldragons@gmail.com>
2022-02-06 21:32:21 +0900

    chore: Iteratorパターン修正

--- iterator/book_shelf.py
+++ iterator/book_shelf.py
@@ -1,32 +1,33 @@
-from Book import Book
+from book import Book
+
 
 class BookShelf():
-    def __init__(self, maxsize: int) -> None:
-        self.__maxsize = maxsize
+    def __init__(self) -> None:
         self.__books: list[Book] = []
-        self.__index :int = 0
 
     def getBookAt(self, index: int) -> Book:
         return self.__books[index]
 
     def appendBook(self, book: Book) -> None:
-        # 最大数を検査
-        if self.getLength() > self.__maxsize:
-            raise IndexError
-
         self.__books.append(book)
 
     def getLength(self) -> int:
         return len(self.__books)
 
     def __iter__(self):
-        return self
+        return BookShelfIterator(self)
+
+
+class BookShelfIterator():
+    def __init__(self, bf: BookShelf):
+        self.__index: int = 0
+        self.bf: BookShelf = bf
 
     def __next__(self) -> Book:
-        if self.__index >= self.getLength():
+        if self.__index >= self.bf.getLength():
             raise StopIteration
 
-        book :Book = self.getBookAt(self.__index)
+        book: Book = self.bf.getBookAt(self.__index)
         self.__index += 1
 
         return book
--- iterator/main.py
+++ iterator/main.py
@@ -1,15 +1,17 @@
-from Book import Book
-from BookShelf import BookShelf
+from book import Book
+from book_shelf import BookShelf
+
 
 def main():
-    bs: BookShelf = BookShelf(4)
+    bs: BookShelf = BookShelf()
     bs.appendBook(Book("Javaで学ぶデザインパターン入門"))
     bs.appendBook(Book("実践Python3"))
     bs.appendBook(Book("リーダブルコード"))
     bs.appendBook(Book("ソフトウェアテストの教科書"))
 
-    for book in iter(bs):
+    for book in bs:
         print(book.name)
+
 
 if __name__ == '__main__':
     main()
